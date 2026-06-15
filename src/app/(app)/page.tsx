887383940d57b14bc5668f1aff54251e97acf769
nikolai <nshemankov@gmail.com>
2026-06-15 23:50:53 +0300

    add loading fallback to home, rackets and top 10 pages

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,41 +1,18 @@
-import {Selection} from "@/components/selection";
-import {RacketSelectionItem} from "@/components/racket-selection-item";
 import {getRackets} from "@/services/get-rackets";
 import {getTop10} from "@/services/get-top10";
+import {Suspense} from "react";
+import {RacketsSelectionContainer} from "@/components/rackets-selection-container";
 
 
 export default async function Home() {
-    const [{isError: isErrorRackets, data: products }, { isError: isErrorTop10, data: top10 }] = await Promise.all([
-        getRackets(),
-        getTop10(),
-    ]);
-
-    if (isErrorTop10 && isErrorRackets) {
-        // TODO: do normal error handling
-        throw new Error('Error during data fetch');
-    }
-
     return (
         <>
-            {isErrorRackets ? null : <Selection title='Ракетки' hrefToAll='/rackets' className='mb-16'>
-                {products!.map((product) =>
-                    <RacketSelectionItem
-                        key={product.id}
-                        id={`${product.id}`}
-                        href={product.imageUrl}
-                        name={product.name} />
-                )}
-            </Selection>}
-            {isErrorTop10 ? null : <Selection title='Top 10'>
-                {top10!.map((product) =>
-                    <RacketSelectionItem
-                        key={product.id}
-                        id={`${product.id}`}
-                        href={product.imageUrl}
-                        name={product.name} />
-                )}
-            </Selection>
-            }
+            <Suspense fallback={<div>Loading rackets</div>}>
+                <RacketsSelectionContainer title="Ракетки" getRackets={getRackets} hrefToAll='/rackets' className='mb-16' />
+            </Suspense>
+            <Suspense fallback={<div>Loading top 10</div>}>
+                <RacketsSelectionContainer title="Top 10" getRackets={getTop10} />
+            </Suspense>
         </>
     );
 }
--- src/app/(app)/top-10/page.tsx
+++ src/app/(app)/top-10/page.tsx
@@ -1,23 +1,11 @@
-import {ReactNode} from "react";
-import {RacketSelectionItem} from "@/components/racket-selection-item";
-import {Selection} from "@/components/selection";
+import {ReactNode, Suspense} from "react";
 import {getTop10} from "@/services/get-top10";
+import {RacketsSelectionContainer} from "@/components/rackets-selection-container";
 
 export default async function Top10Page(): Promise<ReactNode> {
-    const {isError, data: top10 } = await getTop10();
-
-    if (isError) {
-        // TODO: do normal error handling
-        throw new Error('Error during data fetch');
-    }
-
-    return <Selection title='Top 10'>
-        {top10!.map((product) =>
-            <RacketSelectionItem
-                key={product.id}
-                id={`${product.id}`}
-                href={product.imageUrl}
-                name={product.name} />
-        )}
-    </Selection>;
+    return (
+        <Suspense fallback={<div>Loading top 10</div>}>
+            <RacketsSelectionContainer title='Top 10' getRackets={getTop10} />
+        </Suspense>
+    );
 }
