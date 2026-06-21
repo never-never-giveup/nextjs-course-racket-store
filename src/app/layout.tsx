c4e0b59d37645602ce941b78c6586d6e15b4f183
nikolai <nshemankov@gmail.com>
2026-06-22 00:10:36 +0300

    add nextjs top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,16 @@ import './globals.css'
 import { FC } from 'react'
 import { cn } from '@/lib/utils'
 import { Geist } from 'next/font/google'
+import NextTopLoader from 'nextjs-toploader'
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-sans' })
 
 const RootLayout: FC<LayoutProps<'/'>> = ({ children }) => (
   <html lang="en" className={cn('font-sans', geist.variable)}>
-    <body className="w-dvw">{children}</body>
+    <body className="w-dvw">
+      <NextTopLoader />
+      {children}
+    </body>
   </html>
 )
 
