61f9a7246b5257e91c5e6c9ca9044624791ffbdb
nikolai <nshemankov@gmail.com>
2026-06-21 16:46:36 +0300

    add metadata

--- src/app/(app)/rackets/[id]/page.tsx
+++ src/app/(app)/rackets/[id]/page.tsx
@@ -1,7 +1,22 @@
+import type { Metadata } from 'next'
 import { ReactNode, Suspense } from 'react'
 import { RacketContainer } from '@/components/racket/racket-container'
+import { getMetadataRacketById } from '@/services/get-metadata-racket-by-id'
 
 type Props = PageProps<'/rackets/[id]'>
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const { id } = await params
+  const product = await getMetadataRacketById(id)
+  if (!product.data) {
+    return {}
+  }
+
+  return {
+    title: product.data.product.name,
+    description: product.data.product.description,
+  }
+}
 
 export default async function RacketPage({ params }: Props): Promise<ReactNode> {
   const { id } = await params
--- src/app/(app)/rackets/page.tsx
+++ src/app/(app)/rackets/page.tsx
@@ -1,6 +1,12 @@
 import { ReactNode } from 'react'
 import BasicRacketsPage from '@/components/basic-rackets-page'
 import { getRackets } from '@/services/get-rackets'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Ракетки',
+  description: 'Все ракетки в магазине',
+}
 
 export default async function RacketsPage(): Promise<ReactNode> {
   const { isError, data: rackets } = await getRackets(20)
